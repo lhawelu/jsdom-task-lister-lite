cfcb8018856e2324a997cc01ca56b3a0c5951704
Larry Hawelu <lhawelu@gmail.com>
2021-02-25 13:05:49 -0800

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+let form = document.querySelector('form')//get more specific ie. use id or class
+form.addEventListener('submit', handNewTask)
+
+function handNewTask(e) {
+  e.preventDefault();
+  let taskList = document.querySelector('ul')//get more specific ie. use tag
+  addNewTask(e.target['new-task-description'].value, taskList)
+}
+
+//todo create X button for deletion **remember operation order**
+function addNewTask(x,y) {
+  let task = document.createElement('li')
+  task.textContent = x
+  y.appendChild(task)
+}
